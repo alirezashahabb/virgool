29723ca15b1d3f7e11f3b543af10e80a71e7fa25
alirezashahab <shehabalireza@gmail.com>
2025-06-12 06:30:39 -0700

    add userNameValidator

--- src/module/auth/auth.service.ts
+++ src/module/auth/auth.service.ts
@@ -1,10 +1,13 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, UnauthorizedException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { isEmail, isMobilePhone } from 'class-validator';
 import { Repository } from 'typeorm';
 import { OtpEntity } from '../user/entities/otp.entity';
 import { ProfileEntity } from '../user/entities/profile.entity';
 import { UserEntity } from '../user/entities/user.entity';
 import { AuthDto } from './dto/auth.dto';
+import { AuthMethod } from './enum/methos';
+import { AuthType } from './enum/type';
 
 
 @Injectable()
@@ -15,11 +18,48 @@ export class AuthService {
     @InjectRepository(OtpEntity) private otpRepository: Repository<OtpEntity>
 
   ) {
-     
+
   }
-      userExistent(authDto : AuthDto){
-        return authDto;
-      }
+  userExistent(authDto: AuthDto) {
+    const { method, type, username } = authDto;
+
+    switch (type) {
+      case AuthType.Login:
+
+        return this.login(method, username);
+
+      case AuthType.Register:
+
+        return this.register(method, username);
+
+      default:
+        throw new UnauthorizedException();
+    }
+
+  }
+
+  login(methods: AuthMethod, userName: string) {
+    return this.userNameValidator(methods, userName);
+  }
+
+  register(methods: AuthMethod, userName: string) {
+    return this.userNameValidator(methods, userName);
+  }
+
+  userNameValidator(method: AuthMethod, userName: string) {
+    switch (method) {
+      case AuthMethod.Email:
+        if (isEmail(userName)) return userName;
+        throw new BadRequestException('ایمیل نا معتبر هست');
+      case AuthMethod.Phone:
+        if (isMobilePhone(userName, "fa-IR")) return userName;
+        throw new BadRequestException('شماره موبایل نامعتبر هست');
+      case AuthMethod.Username:
+        return userName;
+      default:
+        throw new UnauthorizedException('نام کاربرب وجود ندارد');
+    }
+  }
 }
 
 
